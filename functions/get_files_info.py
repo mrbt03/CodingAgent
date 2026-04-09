ce7011602cb844cabc658f09bb086660598d420b
Mark Ruiz <mrbt4523@gmail.com>
2026-04-09 13:13:38 -0500

    Add get file info printing name, size, and if its a directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,26 @@
+import os
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_absolute = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_absolute, directory))
+
+        # First, check if the target directory is directory
+        if not os.path.isdir(target_dir):
+            f'Error: "{directory}" is not a directory'
+
+        # Then, check if the target directory is within working directory
+        valid_target_dir = os.path.commonpath([working_dir_absolute, target_dir]) == working_dir_absolute
+
+        # If not within working directory, raise error
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        files = os.listdir(target_dir)
+        record = ""
+        for file in files:
+            size = os.path.getsize(os.path.join(target_dir, file))
+            is_dir = os.path.isdir(os.path.join(target_dir, file))
+            record +=f"- {file}: file_size={size} bytes, is_dir={is_dir}\n"
+        return record
+    except Exception as e:
+        return f'Error: {e}'
