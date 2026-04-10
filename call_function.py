fe0a84ffce17da7c38e083e39e2d26ba88c74843
Mark Ruiz <mrbt4523@gmail.com>
2026-04-10 12:33:08 -0500

    Add call_function for llm to map calls to appropriate functions and get response from functions. Using just calculator function (hardcoded as wd) for testing

---
+++ call_function.py
@@ -0,0 +1,58 @@
+from typing import Any
+
+
+from google.genai import types
+from functions import get_files_info
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
+from functions.get_file_content import schema_get_file_content, get_file_content
+import os
+import subprocess
+
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info, schema_run_python_file, schema_write_file, schema_get_file_content],
+)
+
+def call_function(function_call, verbose=False):    
+    try:
+        if verbose:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(f" - Calling function: {function_call.name}")
+        function_map = {
+            "get_file_content": get_file_content,
+            "run_python_file": run_python_file,
+            "write_file": write_file,
+            "get_files_info": get_files_info
+        }
+        function_name = function_call.name or ""
+
+        if function_name not in function_map:
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"error": f"Unknown function: {function_name}"},
+                    )
+                ],
+            )
+        args = dict(function_call.args) if function_call.args else {}
+
+        working_directory = "./calculator"
+
+        function_result = function_map[function_name](working_directory, **args)
+
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
